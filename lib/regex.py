c72cf766cf148bd2f61deea1b8f3977693667fa5
SultanTipa1 <josephngumbaugush@gmail.com>
2024-09-24 07:15:58 +0300

    Adds all files

--- lib/regex.py
+++ lib/regex.py
@@ -3,11 +3,23 @@ import re
 # NOTE: There are only a few tests included, so multiple solutions will work.
 # Feel free to encourage students to find oversights and add tests to this lab!
 
-name = r""
-name_regex = re.compile(name)
+name_regex = re.compile(r"^[A-Z][a-zA-Z'-]*(?: [A-Z][a-zA-Z'-]*)*$")
 
-phone_number = r""
-phone_regex = re.compile(phone_number)
+phone_regex = re.compile(r"^(?:\(\d{3}\)\s|\d{3}[-]?)?\d{3}[-]?\d{4}$")
 
-email_address = r""
-email_regex = re.compile(email_address)
+email_regex = re.compile(r"^[a-zA-Z][\w.-]*@[a-zA-Z]+\.[a-zA-Z]{2,}$")
+
+# name = r""
+# name_regex = re.compile(name)
+
+
+# phone_number = r""
+# phone_regex = re.compile(phone_number)
+
+
+# email_address = r""
+# email_regex = re.compile(email_address)
+
+
+
+
